6ff4514637f6424e400afc028cf1f4ee56228b78
NotEvil <notevil036@gmail.com>
2026-03-21 02:09:44 +0200

    chore(root): Set up build.gradle to multi-module project

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+val projectVersion: String by project
+
+allprojects {
+    apply(plugin = "java")
+
+    group = "global.fujitsu"
+    version = projectVersion
+
+    dependencies {
+        "compileOnly"("org.projectlombok:lombok:1.18.44")
+        "annotationProcessor"("org.projectlombok:lombok:1.18.44")
+
+        "testCompileOnly"("org.projectlombok:lombok:1.18.44")
+        "testAnnotationProcessor"("org.projectlombok:lombok:1.18.44")
+
+        "testImplementation"(platform("org.junit:junit-bom:5.10.0"))
+        "testImplementation"("org.junit.jupiter:junit-jupiter")
+        "testRuntimeOnly"("org.junit.platform:junit-platform-launcher")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform {}
+    }
+}
