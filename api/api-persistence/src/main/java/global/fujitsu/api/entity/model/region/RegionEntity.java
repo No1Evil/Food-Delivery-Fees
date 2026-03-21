d96083ccdd76a187d1a853ecc112d3faa2390fd2
NotEvil <notevil036@gmail.com>
2026-03-21 19:24:44 +0200

    feat(api-persistence): use WmoCode object instead of raw string

--- api/api-persistence/src/main/java/global/fujitsu/api/entity/model/region/RegionEntity.java
+++ api/api-persistence/src/main/java/global/fujitsu/api/entity/model/region/RegionEntity.java
@@ -2,16 +2,12 @@ package global.fujitsu.api.entity.model.region;
 
 import global.fujitsu.api.entity.model.EntityModel;
 import global.fujitsu.api.model.region.RegionName;
+import global.fujitsu.api.model.region.WmoCode;
 import lombok.NonNull;
 
 public record RegionEntity(
     @NonNull Long id,
     @NonNull RegionName name,
-    @NonNull String wmoCode
+    @NonNull WmoCode wmoCode
 ) implements EntityModel {
-    public RegionEntity {
-        if (wmoCode.isBlank()){
-            throw new IllegalArgumentException("WMO code cannot be null");
-        }
-    }
 }
--- api/api-persistence/src/main/java/global/fujitsu/api/repository/region/RegionRepository.java
+++ api/api-persistence/src/main/java/global/fujitsu/api/repository/region/RegionRepository.java
@@ -2,13 +2,14 @@ package global.fujitsu.api.repository.region;
 
 import global.fujitsu.api.entity.model.region.RegionEntity;
 import global.fujitsu.api.model.region.RegionName;
+import global.fujitsu.api.model.region.WmoCode;
 import global.fujitsu.api.repository.Repository;
 import lombok.NonNull;
 
 import java.util.Optional;
 
 public interface RegionRepository extends Repository<RegionEntity> {
-    Optional<String> findWmoCodeByRegionName(@NonNull RegionName regionName);
+    Optional<WmoCode> findWmoCodeByRegionName(@NonNull RegionName regionName);
     
-    Optional<RegionName> findRegionNameByCode(@NonNull String wmoCode);
+    Optional<RegionName> findRegionNameByCode(@NonNull WmoCode wmoCode);
 }
