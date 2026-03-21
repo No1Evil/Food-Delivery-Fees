40c867cc8374f2d20a3f72656eb891aa69004c8d
NotEvil <notevil036@gmail.com>
2026-03-21 16:23:35 +0200

    feat(api-persistence): RegionalFeeRepository interface implementation

---
+++ api/api-persistence/src/main/java/global/fujitsu/api/repository/fee/RegionalFeeRepository.java
@@ -0,0 +1,18 @@
+package global.fujitsu.api.repository.fee;
+
+import global.fujitsu.api.model.entity.fee.RegionalBaseFee;
+import global.fujitsu.api.model.entity.region.Region;
+import global.fujitsu.api.model.entity.vehicle.VehicleType;
+import global.fujitsu.api.model.region.RegionName;
+import lombok.NonNull;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+public interface RegionalFeeRepository extends FeeRepository<RegionalBaseFee> {
+    Optional<BigDecimal> findBaseFee(@NonNull RegionName regionName, @NonNull VehicleType vehicleType);
+
+    default Optional<BigDecimal> findBaseFee(@NonNull Region region, @NonNull VehicleType vehicleType){
+        return findBaseFee(region.name(), vehicleType);
+    }
+}
