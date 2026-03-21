8eaab17df9bad6f8d20af86743f3f154a2267eb7
NotEvil <notevil036@gmail.com>
2026-03-21 17:51:22 +0200

    feat(api-domain): implement WindSpeedFeeService using BaseService

---
+++ api/api-domain/src/main/java/global/fujitsu/api/domain/service/WindSpeedFeeService.java
@@ -0,0 +1,8 @@
+package global.fujitsu.api.domain.service;
+
+import global.fujitsu.api.domain.dto.request.create.CreateWindSpeedFeeRequest;
+import global.fujitsu.api.domain.dto.response.get.WindSpeedFeeResponse;
+import global.fujitsu.api.domain.service.base.BaseService;
+
+public interface WindSpeedFeeService extends BaseService<WindSpeedFeeResponse, CreateWindSpeedFeeRequest> {
+}
