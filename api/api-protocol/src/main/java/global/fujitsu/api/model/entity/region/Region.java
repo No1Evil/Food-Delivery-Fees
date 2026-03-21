93fe287645e30e09c1cfa9eb70f7af2115e37160
NotEvil <notevil036@gmail.com>
2026-03-21 15:49:07 +0200

    refactor(api-protocol): changed RegionName property name: regionName -> name

--- api/api-protocol/src/main/java/global/fujitsu/api/model/entity/region/Region.java
+++ api/api-protocol/src/main/java/global/fujitsu/api/model/entity/region/Region.java
@@ -4,7 +4,7 @@ import global.fujitsu.api.model.entity.EntityModel;
 import global.fujitsu.api.model.region.RegionName;
 import lombok.NonNull;
 
-public record Region(@NonNull RegionName regionName, @NonNull String wmoCode) implements EntityModel {
+public record Region(@NonNull RegionName name, @NonNull String wmoCode) implements EntityModel {
     public Region {
         if (wmoCode.isBlank()){
             throw new IllegalArgumentException("WMO code cannot be null");
