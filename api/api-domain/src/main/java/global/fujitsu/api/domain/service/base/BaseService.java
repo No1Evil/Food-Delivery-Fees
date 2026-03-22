d13a5ef7289e647444ee24b2b9f42642a0be76fd
NotEvil <notevil036@gmail.com>
2026-03-23 00:08:33 +0200

    refactor(api-domain): changed package imports of BaseService

--- api/api-domain/src/main/java/global/fujitsu/api/domain/service/base/BaseService.java
+++ api/api-domain/src/main/java/global/fujitsu/api/domain/service/base/BaseService.java
@@ -1,7 +1,7 @@
 package global.fujitsu.api.domain.service.base;
 
-import global.fujitsu.api.model.dto.request.create.CreateRequest;
-import global.fujitsu.api.model.dto.response.get.GetResponse;
+import global.fujitsu.api.model.dto.request.base.CreateRequest;
+import global.fujitsu.api.model.dto.response.base.GetResponse;
 import lombok.NonNull;
 
 import java.util.List;
