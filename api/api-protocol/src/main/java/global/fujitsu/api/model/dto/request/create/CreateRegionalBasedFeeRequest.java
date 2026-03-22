8ee6299e5030f76ba1d3957f4360831c3f0b3045
NotEvil <notevil036@gmail.com>
2026-03-23 00:17:42 +0200

    feat(api-protocol): CreateRegionalBasedFeeRequest changed properties of regionName & vehicleType to regionId & vehicleTypeId

--- api/api-protocol/src/main/java/global/fujitsu/api/model/dto/request/create/CreateRegionalBasedFeeRequest.java
+++ api/api-protocol/src/main/java/global/fujitsu/api/model/dto/request/create/CreateRegionalBasedFeeRequest.java
@@ -1,15 +1,13 @@
 package global.fujitsu.api.model.dto.request.create;
 
-import global.fujitsu.api.model.region.RegionName;
-import global.fujitsu.api.model.vehicle.VehicleType;
+import global.fujitsu.api.model.dto.request.base.CreateRequest;
 import lombok.NonNull;
 
 import java.math.BigDecimal;
 
-public record CreateRegionalBaseFeeRequest(
-    @NonNull RegionName regionName,
-    @NonNull VehicleType vehicleType,
 public record CreateRegionalBasedFeeRequest(
+    @NonNull Long regionId,
+    @NonNull Long vehicleTypeId,
     @NonNull BigDecimal fee,
     @NonNull Boolean isAllowed
 ) implements CreateRequest {
