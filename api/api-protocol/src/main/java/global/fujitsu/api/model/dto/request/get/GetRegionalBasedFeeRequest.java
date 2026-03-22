9731b83ec746c0ccac4ff186db09cdb38f530cde
NotEvil <notevil036@gmail.com>
2026-03-23 00:58:15 +0200

    refactor(api-protocol): GetRegionalFeeRequest renamed to GetRegionalBasedFeeRequest

--- api/api-protocol/src/main/java/global/fujitsu/api/model/dto/request/get/GetRegionalBasedFeeRequest.java
+++ api/api-protocol/src/main/java/global/fujitsu/api/model/dto/request/get/GetRegionalBasedFeeRequest.java
@@ -4,7 +4,7 @@ import global.fujitsu.api.model.dto.request.base.GetFeeRequest;
 import global.fujitsu.api.model.region.RegionName;
 import lombok.NonNull;
 
-public record GetRegionalFeeRequest(
+public record GetRegionalBasedFeeRequest(
     @NonNull Long vehicleTypeId,
     @NonNull RegionName regionName
 ) implements GetFeeRequest {
