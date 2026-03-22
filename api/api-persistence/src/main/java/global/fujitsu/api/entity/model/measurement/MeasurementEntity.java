ed7271404cf0aaea1642e38583aa93aaa78b60ef
NotEvil <notevil036@gmail.com>
2026-03-22 03:40:54 +0200

    feat(api-persistence): MeasurementEntity added mapper fromResultSet()

--- api/api-persistence/src/main/java/global/fujitsu/api/entity/model/measurement/MeasurementEntity.java
+++ api/api-persistence/src/main/java/global/fujitsu/api/entity/model/measurement/MeasurementEntity.java
@@ -1,11 +1,12 @@
 package global.fujitsu.api.entity.model.measurement;
 
 import global.fujitsu.api.entity.model.EntityModel;
-import global.fujitsu.api.model.region.RegionName;
 import lombok.NonNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.Instant;
 
 /** @param weatherPhenomenon weather type */
@@ -23,5 +24,16 @@ public record MeasurementEntity(
         if (weatherPhenomenon.isBlank()){
             throw new IllegalArgumentException("Weather phenomenon cannot be empty");
         }
+    }
+
+    public static MeasurementEntity fromResultSet(ResultSet rs) throws SQLException {
+        return new MeasurementEntity(
+            rs.getLong("id"),
+            rs.getLong("region_id"),
+            rs.getBigDecimal("air_temperature"),
+            rs.getBigDecimal("wind_speed"),
+            rs.getString("weather_phenomenon"),
+            rs.getTimestamp("measured_at").toInstant()
+        );
     }
 }
