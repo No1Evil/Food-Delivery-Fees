7b645d840f4e08e20d04b5e2b44f695e6200ca17
NotEvil <notevil036@gmail.com>
2026-03-22 17:47:35 +0200

    feat(persistence-impl): implemented JdbcVehicleTypeDao

---
+++ implementations/persistence-impl/src/main/java/global/fujitsu/persistence/dao/impl/JdbcVehicleTypeDao.java
@@ -0,0 +1,44 @@
+package global.fujitsu.persistence.dao.impl;
+
+import global.fujitsu.api.entity.model.vehicle.VehicleTypeEntity;
+import global.fujitsu.api.repository.vehicle.VehicleTypeRepository;
+import global.fujitsu.persistence.dao.BaseJdbcDao;
+import global.fujitsu.persistence.util.SqlConstants;
+import jakarta.annotation.PostConstruct;
+import lombok.NonNull;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public final class JdbcVehicleTypeDao
+    extends BaseJdbcDao<VehicleTypeEntity>
+    implements VehicleTypeRepository {
+
+    public JdbcVehicleTypeDao(@NonNull JdbcTemplate jdbcTemplate) {
+        super(jdbcTemplate,
+            "vehicle_types",
+            List.of("id", "type"),
+            VehicleTypeEntity.class
+        );
+    }
+
+    @Override
+    public Optional<VehicleTypeEntity> findByTypeName(@NonNull String name) {
+        String sql = SqlConstants.FIND_BY_QUERY("vehicle_types", "type");
+        return jdbcTemplate.query(sql, mapper, name)
+            .stream().findFirst();
+    }
+
+    @Override
+    protected PreparedStatement prepareSaveStatement(PreparedStatement ps, VehicleTypeEntity entity) throws SQLException {
+        ps.setString(1, entity.type().toString());
+        return ps;
+    }
+}
