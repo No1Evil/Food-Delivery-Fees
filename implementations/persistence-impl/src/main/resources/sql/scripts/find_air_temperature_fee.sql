da356ade51f166652742fba7683b51a36b83fe96
NotEvil <notevil036@gmail.com>
2026-03-23 03:53:34 +0200

    refactor(persistence-impl): find_air_temperature_fee.sql usage of table aliases

--- implementations/persistence-impl/src/main/resources/sql/scripts/find_air_temperature_fee.sql
+++ implementations/persistence-impl/src/main/resources/sql/scripts/find_air_temperature_fee.sql
@@ -1,5 +1,5 @@
-select * from air_temperature_fees
-where (vehicle_type_id = ? or vehicle_type_id is null)
-    and ? between min_temperature and max_temperature
-order by vehicle_type_id desc nulls last
+select * from air_temperature_fees t
+where (t.vehicle_type_id = ? or t.vehicle_type_id is null)
+    and ? between t.min_temperature and t.max_temperature
+order by t.vehicle_type_id desc nulls last
 limit 1
