63762aaa68dc498d46062203c2cbba965dcf2a44
NotEvil <notevil036@gmail.com>
2026-03-23 03:47:13 +0200

    feat(api-persistence): MeasurementRepository findLatest renamed to find, accepted arg is GetMeasurementRequest, updated Javadoc

--- api/api-persistence/src/main/java/global/fujitsu/api/repository/measurement/MeasurementRepository.java
+++ api/api-persistence/src/main/java/global/fujitsu/api/repository/measurement/MeasurementRepository.java
@@ -1,15 +1,16 @@
 package global.fujitsu.api.repository.measurement;
 
 import global.fujitsu.api.entity.model.measurement.MeasurementEntity;
-import global.fujitsu.api.model.region.RegionName;
+import global.fujitsu.api.model.dto.request.get.GetMeasurementRequest;
 import global.fujitsu.api.repository.base.Repository;
+import lombok.NonNull;
 
-import java.time.Instant;
 import java.util.Optional;
 
 public interface MeasurementRepository extends Repository<MeasurementEntity> {
-    /** {@return actual measurement at the specified time }  */
-    Optional<MeasurementEntity> findLatest(RegionName regionName, Instant timestamp);
+
+    /** {@return measurement at the specified time or else current server time}*/
+    Optional<MeasurementEntity> find(@NonNull GetMeasurementRequest request);
 
     /* TODO
     /** {@return found measurement list}  /
