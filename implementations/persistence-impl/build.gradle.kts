bf082f99dd9b8f53fc912b244c53c2cb1e62fd6f
NotEvil <notevil036@gmail.com>
2026-03-26 10:37:49 +0200

    chore(persistence-impl): add boot-test-autoconfigure

--- implementations/persistence-impl/build.gradle.kts
+++ implementations/persistence-impl/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    testImplementation("org.springframework.boot:spring-boot-test-autoconfigure")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     runtimeOnly("com.h2database:h2")
 }
