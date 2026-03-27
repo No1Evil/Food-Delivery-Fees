d7847cbb0a8e58beb88b26b54a2de0acc871d4f5
NotEvil <notevil036@gmail.com>
2026-03-27 23:13:18 +0200

    refactor(rest-app): move MeasurementController to /controller/admin

--- rest-app/src/main/java/global/fujitsu/restapp/controller/admin/MeasurementController.java
+++ rest-app/src/main/java/global/fujitsu/restapp/controller/admin/MeasurementController.java
@@ -1,53 +1,54 @@
-package global.fujitsu.restapp.controller;
+package global.fujitsu.restapp.controller.admin;
 
 import global.fujitsu.api.domain.service.MeasurementService;
 import global.fujitsu.api.model.dto.request.create.CreateMeasurementRequest;
 import global.fujitsu.api.model.dto.request.get.GetMeasurementRequest;
 import global.fujitsu.api.model.dto.response.get.MeasurementResponse;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Provides API for measurements.
  */
 @RestController
-@RequestMapping("api/v1/admin/measurements")
+@RequestMapping(value = "/api/admin/measurements", headers = "X-API-VERSION=1")
 @RequiredArgsConstructor
 public class MeasurementController {
 
   private final MeasurementService service;
 
-  /** {@return found measurement} */
+  /** {@return measurement list or exact by request} */
   @GetMapping
-  public ResponseEntity<MeasurementResponse> findMeasurement(
-      @RequestBody GetMeasurementRequest request) {
-    return ResponseEntity.ok(service.find(request));
+  public ResponseEntity<?> find(
+      @RequestBody(required = false) GetMeasurementRequest request) {
+    if (request != null) {
+      return ResponseEntity.ok(service.find(request));
+    }
+    return ResponseEntity.ok(service.findAll());
   }
 
   /** {@return created measurement id} */
   @PostMapping
-  public ResponseEntity<Long> createMeasurement(
+  public ResponseEntity<Long> create(
       @RequestBody CreateMeasurementRequest request) {
     return ResponseEntity.ok(service.create(request));
   }
 
-  /** {@return if measurement was deleted} */
-  @DeleteMapping
-  public ResponseEntity<Boolean> deleteMeasurementById(@RequestParam Long id) {
-    return ResponseEntity.ok(service.delete(id));
+  @GetMapping("/{id}")
+  public ResponseEntity<MeasurementResponse> findById(@PathVariable Long id) {
+    return ResponseEntity.ok(service.findById(id));
   }
 
-  /** {@return list of existing measurements} */
-  @GetMapping
-  public ResponseEntity<List<MeasurementResponse>> findAllMeasurements() {
-    return ResponseEntity.ok(service.findAll());
+  /** {@return if measurement was deleted} */
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Boolean> deleteById(@PathVariable Long id) {
+    return ResponseEntity.ok(service.delete(id));
   }
 }
