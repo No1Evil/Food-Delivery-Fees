1f97adc7d7836ea1a14b45481984ac0faf41a497
NotEvil <notevil036@gmail.com>
2026-03-29 02:43:55 +0200

    feat(rest-app): enable scheduling in RestApplication

--- rest-app/src/main/java/global/fujitsu/restapp/RestApplication.java
+++ rest-app/src/main/java/global/fujitsu/restapp/RestApplication.java
@@ -2,7 +2,9 @@ package global.fujitsu.restapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = "global.fujitsu")
 public class RestApplication {
   static void main(String[] args){
