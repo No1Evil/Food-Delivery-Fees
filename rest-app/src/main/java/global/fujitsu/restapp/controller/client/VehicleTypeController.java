6719ab6ba2072c4821c9c5ec2f18783ac494b74b
NotEvil <notevil036@gmail.com>
2026-03-30 16:42:08 +0300

    feat(rest-app): implement VehicleTypeController with public endpoints findAll and findByName

---
+++ rest-app/src/main/java/global/fujitsu/restapp/controller/client/VehicleTypeController.java
@@ -0,0 +1,36 @@
+package global.fujitsu.restapp.controller.client;
+
+import global.fujitsu.api.domain.service.VehicleTypeService;
+import global.fujitsu.api.model.dto.response.get.VehicleTypeResponse;
+import global.fujitsu.api.model.vehicle.VehicleType;
+import jakarta.validation.Valid;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/** Provides public endpoint to vehicle types. */
+@RestController
+@RequestMapping(value = "/public/vehicles", headers = "X-API-VERSION=1")
+@RequiredArgsConstructor
+public class VehicleTypeController {
+
+  private final VehicleTypeService service;
+
+  /** {@return found vehicle types} */
+  @GetMapping
+  public ResponseEntity<List<VehicleTypeResponse>> findAll() {
+    return ResponseEntity.ok(service.findAll());
+  }
+
+  /** {@return found vehicle type} */
+  @GetMapping("/find")
+  public ResponseEntity<VehicleTypeResponse> findByName(@RequestParam String vehicleType) {
+    return ResponseEntity.ok(service.findByName(new VehicleType(vehicleType)));
+  }
+}
